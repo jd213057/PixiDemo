af19b1d3b5fa0f93be19e5a79f83e43d7744324a
jd213057 <jonathan.dimur@gmail.com>
2020-10-06 21:42:04 +0200

    v1.0.0.0: configuration prettier + vscode + debut integration pixijs

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
-const livereload = require("livereload");
-const connectLivereload = require("connect-livereload");
+const livereload = require('livereload');
+const connectLivereload = require('connect-livereload');
 var express = require('express');
 var app = express();
 const port = 3000;
 var url = 'http://localhost:' + port;
 const opn = require('opn');
 
-
 console.log('Connecting to local server : http://localhost:3000...');
 
 const dirUrl = __dirname + '/public';
 const liveReloadServer = livereload.createServer();
 liveReloadServer.watch(__dirname);
-liveReloadServer.server.once("connection", () => {
-    setTimeout(() => {
-      liveReloadServer.refresh("/");
-    }, 100);
-  });
+liveReloadServer.server.once('connection', () => {
+	setTimeout(() => {
+		liveReloadServer.refresh('/');
+	}, 100);
+});
 // ligne 17 à activer pour relaunch sur autosave
 app.use(connectLivereload());
-app.use('/assets', express.static('assets'));
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+app.use('/static', express.static('static'));
+app.get('/', function (req, res) {
+	res.sendFile(dirUrl + '/index.html');
 });
 
-
-app.get('/', function(req, res) {
-    res.redirect('index.html');
+app.get('/', function (req, res) {
+	res.redirect('index.html');
 });
 
 app.listen(port);
 console.log('Connection successful !');
 console.log('Opening this link ' + url + ' in you Web Browser...');
 opn(url);
+
